af32b4aee1ca3740d191c02becf81f0468191fa0
Max Lerman <mjl82601@gmail.com>
2024-07-15 19:30:51 +0100

    feat: dabbled with s3 bucket creation

--- cdk-starter/lib/cdk-starter-stack.ts
+++ cdk-starter/lib/cdk-starter-stack.ts
@@ -1,9 +1,50 @@
 import * as cdk from "aws-cdk-lib";
+import { Duration } from "aws-cdk-lib";
+import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration: number) {
+    super(scope, id);
+
+    new Bucket(this, "L3Bucket", {
+      lifecycleRules: [
+        {
+          expiration: Duration.days(expiration),
+        },
+      ],
+    });
+  }
+}
 
 export class CdkStarterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    //* create an s3 bucket 3 ways:
+
+    //* 1. L1 construct
+    new CfnBucket(this, "MyL1Bucket", {
+      lifecycleConfiguration: {
+        rules: [
+          {
+            expirationInDays: 1,
+            status: "Enabled", //? status: "Enabled" is the default in L2
+          },
+        ],
+      },
+    });
+
+    //* 2. L2 construct - more flexible configuration
+    new Bucket(this, "MyL2Bucket", {
+      lifecycleRules: [
+        {
+          expiration: Duration.days(2), //? objects expire 2 days after creation
+        },
+      ],
+    });
+
+    //* 3. L3 construct - using custom class that extends Construct class
+    new L3Bucket(this, "MyL3Bucket", 3);
   }
 }
